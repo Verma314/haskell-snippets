31b6955b6fbd216f1f98e3a016ebc0c515c765c9
Aditya Verma <asverma314@gmail.com>
2020-12-11 19:40:11 +0530

    added quick sort

---
+++ 02messyinput.hs
@@ -0,0 +1,24 @@
+messyMain :: IO()
+
+messyMain = do
+    print "Who is the recipient?"
+    recipient <- getLine    
+    print "What is the title"
+    title <- getLine
+    print "Who is the author"
+    author <- getLine
+    print ( "Dear " ++ recipient ++ " \n Thank you for buying " ++ title ++ " ++ written by " ++ author  )
+
+
+
+
+quicksort [] = []
+quicksort (x: xs) = lesser ++ [x] ++ greater
+    where 
+        lesser = filter ( < x ) xs
+        greater = filter ( >= x) xs 
+
+
+main :: IO()
+main = do
+    print "do nothing"
