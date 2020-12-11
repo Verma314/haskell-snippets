148301241eeff813bb79532e0e18a7bf61b04cf4
Aditya Verma <asverma314@gmail.com>
2020-12-12 03:48:47 +0530

    updating overwriting doc

--- 06letWhereOverwriteVars.hs
+++ 06letWhereOverwriteVars.hs
@@ -24,7 +24,53 @@ x = 100
 overwrite4 x = x + 10
             where x = 1
 
+mysum y = y + x             
+
 -- whatever x we pass to overwrite4 it gets overwritten by 1
 
 
+
+
+{-
+
+implement this function using lambdas:
+
+counter x = let x = x + 1
+            in
+             let x = x + 1
+             in
+              x
+-}
+
+counter x =  (\ x ->   ( \x -> x ) x + 1 ) x + 1 
+
+
+
+-- more fun with overwriting stuff:
+
+{-
+
+implement this function using lambdas:
+
+counter x = let x = x + 2
+            in
+             let x = x + 3
+             in
+              let x = x + 4
+                in 
+                    x
+-}
+
+counter2 x =  (\ x ->  (  \x -> ( \x -> x ) x + 4 ) x + 3 ) x + 2 
+
+
+
+-- doesn't work and stays stuck. why does it stay stuck?
+counter3 x = let x = x + 1
+            in
+             let x = x + 1
+             in
+              let x = x + 1
+                in 
+                    x
 
