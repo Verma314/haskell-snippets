db145122f0c70ece5d409bc4efdf8ac37c70c7ce
Aditya Verma <asverma314@gmail.com>
2020-12-12 00:56:55 +0530

    more lambdas

--- 05lambdas.hs
+++ 05lambdas.hs
@@ -7,3 +7,21 @@ evenOddChecker x = if even x then
                     x - 2
                     else
                     (3 * x )  + 1 
+
+double =  \x -> (2*x) 
+
+
+-- sqSum or sumSq challenge
+
+-- given:
+
+sumSqOrSqSum x y =  if sumSq > sqSum then sumSq
+                    else sqSum
+        where sumSq = ( x ^ 2  ) + ( y ^ 2 )
+              sqSum  = ( x + y) ^ 2
+
+
+--soln:
+
+sumSqOrSqSum2 x y = (\ sumSq sqSum -> if sumSq > sqSum  then sumSq
+                                else sqSum) ( (x ^ 2) + (y ^2)) ((x + y) ^ 2)
