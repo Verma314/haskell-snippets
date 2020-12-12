77b192aa4be6fea4d865d87e48060a0b79fdf127
Aditya Verma <asverma314@gmail.com>
2020-12-12 17:13:49 +0530

    first class functions

--- 07firstClassFxns.hs
+++ 07firstClassFxns.hs
@@ -1,1 +1,35 @@
 -- 
+
+ifEvenApplyFxn x fxn = if mod x 2 == 0 
+                        then fxn x
+                        else x
+
+
+
+doublex x = 2 * x
+
+
+
+main :: IO()
+main = do
+    print (ifEvenApplyFxn 10 doublex)
+    print (ifEvenApplyFxn 9 doublex)
+
+ 
+ --z = ifEvenApplyFxn 9 doublex
+
+
+
+
+filerLessThan y (x:xs) = if x < y then x : filerLessThan y xs
+                         else filerLessThan y xs
+     
+
+exp2 :: Ord a => a -> [a] -> [a] -- why do we *have* to write this line?
+exp2 _ [] = []
+exp2 y (x:xs)  =   if x < y then  exp2 y xs
+                else  x : exp2 y xs
+
+
+
+
