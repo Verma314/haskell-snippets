da75ce315ce0f42f855e73b56573bfdd8d0e9561
Aditya Verma <asverma314@gmail.com>
2020-12-13 11:51:10 +0530

    more closures

--- 09closures.hs
+++ 09closures.hs
@@ -7,6 +7,8 @@ ifEvenDouble n = if even n
                  else n
 
 
+
+
 -- functions as arguments:
 ifEvenApplyFunc n func = if even n
                          then func n
@@ -14,11 +16,24 @@ ifEvenApplyFunc n func = if even n
 
 isEvenInc2 n = ifEvenApplyFunc n (\x -> x + 1 )
 
-
 -- What we want is a function that builds ifEvenX functions.
 genIfEvenX myCustomFunc =  ( \x -> ifEvenApplyFunc x myCustomFunc ) 
 
-
-
 --  a function genIfXEven that creates 'a closure with x' and returns a new function that allows the user to pass in a function to apply to x if x is even.
 genIfXEven x = (\ givenFunc -> ifEvenApplyFunc x givenFunc)
+
+
+
+-- write it all from scratch
+
+myinc =  \x -> x + 1 
+
+-- write ifEvenThenApply func using a closure on func
+ifEvenApplyFuncClosure func = (\ x -> if even x then func x else x)
+-- even using this function alone would do ^, takes a custom function
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -9,3 +9,7 @@
 ```
 myFunction x y = (\ x y -> if x > y then x + y else x) (x ^ 2) (y ^ 2)
 ```
+* "We are passing in a function and returning a lambda function. The function func that we passed in is captured inside the lambda function. When we capture a value inside a lambda function, this is referred to as a closure (on the function func). (on the u" (From Will Kurt's Get Programming in Haskell book)
+```
+ifEvenApplyFuncClosure func = (\ x -> if even x then func x else x)
+```
