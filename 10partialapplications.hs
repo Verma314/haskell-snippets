e8d716ef31ff55511f996ed9e6c2eaf09302c4c4
Aditya Verma <asverma314@gmail.com>
2020-12-14 12:25:30 +0530

    added partial functions

---
+++ 10partialapplications.hs
@@ -0,0 +1,105 @@
+add4 a b c d = a + b + c + d  
+
+-- usually what we would do:
+
+
+addXto3 x = (\b c d ->
+              add4 x b c d)
+
+
+-- now we can pass a function a partial list of arguments, and it generates a closure (?) [not sure if i am using the right vocab here]
+-- the new 'mystery' function generated has already captured one element in it, and will acceept the remaining 3 elements later when called              
+mystery = add4 3
+
+-- same as this
+
+mysterySame = addXto3 3
+
+-- this language feature is called partial application
+mystery2 = add4 3 2 1 
+
+mysterySame2 = mysterySame 1 2 3
+
+
+-------------------------------------------------------------------------------------------------------------------
+
+-------------------------------------------- Exercises Will Kurt 5.3.1 --------------------------------------------
+
+
+createUrl host resource apiKey id = "http://" ++ host ++ "/" ++ resource ++ "/" ++ apiKey ++ "/" ++ id
+
+
+googleHostBookResourceApiKeyRand = createUrl "google.com" "book" "42jh34bm23"
+-- when doing a partial application you may choose to not send in any paramters to the function
+
+googleHostBookResource apiKey = createUrl "google.com" "book" apiKey
+
+--example, application:
+uriCretorIdUnset = googleHostBookResource "qvehjq23v42" 
+
+test = uriCretorIdUnset "1"
+
+------------------------------------------------------------------------------------------------------------------------
+
+-- swapping arguments: 
+
+generateValue childVal parentVal = parentVal ++ " " ++ childVal
+
+-- lets assume that the above is a library function that we cant edit:
+
+generatedValuesSwapped parent child = generateValue child parent
+-- created a partial-function-friendly function above ^
+-- now we have a fxn which has an argument list from more to less generalized
+
+-- lets test it out:
+parentEquals1ChildUnset = generatedValuesSwapped "1"
+
+test1 = parentEquals1ChildUnset "Child"
+
+
+-- the above is a one time solution. But not scalable if we have n number of functions whose parameters need to be swapped
+
+-- let us write a generalized function which can take any function, flip its arguments, and then return the new function
+
+flipBinaryArgs myfunction = (\x y -> myfunction y x)
+
+-- now lets fix our intiial function 
+-- generateValue childVal parentVal = parentVal ++ " " ++ childVal
+
+generateValueV2 = flipBinaryArgs generateValue
+
+-- we can test it out
+parentFixedAs1ChildUnset = generateValueV2 "1"
+
+
+{--
+
+flipBinaryArgs myfunction = (\x y -> myfunction y x)
+
+This function 
+1. is a closure because it takes in a function myFunction and 'traps' it as a paramter. The new resultant fxn can take it 2 params x, y
+2. demonstrats 1st class functions. As this function accepts a function in the paramters 
+3. uses lambda expressions.
+
+-}
+
+----------------------------------- Exercise 5.4 ------------------------------------------
+
+{-
+Use flip and partial application to create a function called subtract2 that removes 2 from whatever number is passed in to it.
+-}
+
+subtract_  a b = (-) a b 
+flipSubtractParamters = flip subtract_
+solution a = flipSubtractParamters 2 a
+
+--- or
+
+flipedNegativeSign = flip (-)
+subtract2Alt = flipedNegativeSign 2 
+test2 b = subtract2Alt b
+
+
+--- or
+
+flipedNegativeSign2 = flip (-) 2
--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # haskell-snippets
 
-
-
 ### One liners 
 
 * Let and Where are both ways of creating variables. Choosing to use let or where is a matter of style the vast majority of the time in Haskell. 
@@ -13,3 +11,25 @@ myFunction x y = (\ x y -> if x > y then x + y else x) (x ^ 2) (y ^ 2)
 ```
 ifEvenApplyFuncClosure func = (\ x -> if even x then func x else x)
 ```
+* Partial functions are critical
+```
+> add4 a b c d = a + b + c + d  
+> adderPlus3 = add4 3
+> adderPlus3 1 2 3
+9
+```
+* Partial functions are the reason why arguments should be ordered from most to least general.
+
+*  We can write a function which can take in a function as an argument, and flips the order of the function's argument 
+```
+flipBinaryArgs myfunction = (\x y -> myfunction y x)
+```
+
+* A built in Haskell function also exists called ```flip``` to flip binary arguments. To flip negative binary operator:
+```
+> flipedNegativeSign = flip (-)
+> test = flip (-) 1 10
+9
+```
+* "Closures combine lambda functions and first-class functions to give you amazing power." Will Kurt.
+
