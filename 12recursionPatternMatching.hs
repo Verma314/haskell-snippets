480b432885f6c878461389bde00697e15d19c218
Aditya Verma <asverma314@gmail.com>
2020-12-15 11:35:16 +0530

    added recursion

--- 12recursionPatternMatching.hs
+++ 12recursionPatternMatching.hs
@@ -1,1 +1,9 @@
--- todo
+-- Implement 'take
+-- Example:
+-- take 3 [1,2,3,4]
+-- [1,2,3]
+
+myTake num (elem:lis) = if num > (length (lis) + 1) then (elem:lis) -- edge case:
+                        else if num == 0 then []
+                        else elem : (myTake (num - 1) lis) 
+                
