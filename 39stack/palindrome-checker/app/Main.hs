c9e50fffb12dfc9387c0ac260c5571c30fdb6520
Aditya Verma <asverma314@gmail.com>
2021-01-22 23:57:33 +0530

    Adding code to project, modifying project config in cabal

--- 39stack/palindrome-checker/app/Main.hs
+++ 39stack/palindrome-checker/app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Lib
+import Data.Text as T
+import Data.Text.IO as TIO
 
 main :: IO ()
-main = someFunc
+main = do
+  TIO.putStrLn "Enter a word and I'll let you know if it's a palindrome!"
+  text <- TIO.getLine
+  let response = if isPalindrome text
+                 then "it is!"
+                 else "it's not!"
+  TIO.putStrLn response
--- 39stack/palindrome-checker/src/Lib.hs
+++ 39stack/palindrome-checker/src/Lib.hs
@@ -1,6 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
 module Lib
-    ( someFunc
+    ( isPalindrome
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import qualified Data.Text as T
+import Data.Char (toLower,isSpace,isPunctuation)
+
+stripWhiteSpace :: T.Text -> T.Text
+stripWhiteSpace text = T.filter (not . isSpace) text
+
+stripPunctuation :: T.Text -> T.Text
+stripPunctuation text = T.filter (not . isPunctuation) text
+
+preProcess :: T.Text -> T.Text
+preProcess = stripWhiteSpace . stripPunctuation . T.toLower
+
+isPalindrome :: T.Text -> Bool
+isPalindrome text = cleanText == T.reverse cleanText
+  where cleanText = preProcess text
