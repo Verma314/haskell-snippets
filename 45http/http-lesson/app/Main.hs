e967e435fa2a990e0cf65f11cf9d85f6732cbea8
Aditya Verma <asverma314@gmail.com>
2021-01-28 00:53:29 +0530

    Making a simple HTTP GET call

--- 45http/http-lesson/app/Main.hs
+++ 45http/http-lesson/app/Main.hs
@@ -20,6 +20,41 @@ apiPath = "/cdo-web/api/v2/datasets"
 
 
 
+buildRequestOld :: BC.ByteString -> BC.ByteString -> BC.ByteString
+                    -> BC.ByteString -> Request
+buildRequestOld token host method path =  setRequestMethod method 
+                                        (setRequestHost host
+                                            (setRequestHeader "token" [myToken]
+                                            (setRequestPath path
+                                                (setRequestSecure True
+                                                        (setRequestPort 443 defaultRequest )))))
+
+
+buildRequest :: BC.ByteString -> BC.ByteString -> BC.ByteString
+             -> BC.ByteString -> Request
+buildRequest token host method path  = setRequestMethod method
+                                  $ setRequestHost host
+                                  $ setRequestHeader "token" [token]
+                                  $ setRequestPath path
+                                  $ setRequestSecure True
+                                  $ setRequestPort 443
+                                  $ defaultRequest
+
+
+
+
+request :: Request
+request = buildRequest myToken noaaHost "GET" apiPath
+-- now that we have built the request,
+-- let us execute.
 
 main :: IO ()
-main = print "Hi"
+main = do
+  response <- httpLBS request
+  let status = getResponseStatusCode response
+  if status == 200
+    then do
+         print "saving request to file"
+         let jsonBody = getResponseBody response
+         L.writeFile "data.json" jsonBody
+    else print "request failed with error"
