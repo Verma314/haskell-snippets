95bcecc9cd8ec67fae08c363ae85f2ee40e95d5d
Aditya Verma <asverma314@gmail.com>
2021-01-31 23:36:17 +0530

    more db functions

--- 47db/db-lesson/app/Main.hs
+++ 47db/db-lesson/app/Main.hs
@@ -11,6 +11,67 @@ addUser username =  do
                     execute conn "INSERT INTO users (username) VALUES (?)" (Only username)
                     print "user added"
                     close conn
+-- takes in the db value , and an IO action
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName operation = do
+                            conn <- open dbName
+                            operation conn 
+                            close conn
+
+addUserOperation ::  String -> Connection -> IO () 
+addUserOperation user conn = do
+        execute conn "INSERT INTO users (username) VALUES (?)" (Only user)
+        putStrLn "user added"       
+
+{-
+How to use:
+> withConn "tools.db" (addUserOperation "Zezima")
+user added
+
+
+-- here ^ you are creating a partial function, and using complicated operations, let us simplify
+-}        
+
+addUserNew :: String -> IO ()
+addUserNew user = withConn "tools.db" $
+                        (\ conn ->  do 
+                                    execute conn  "INSERT INTO users (username) VALUES (?)" (Only user)
+                                    putStrLn "added user"
+                                    )
+
+-- lambdas are tedious, so we create,
+executeWrapper :: ToRow q =>  Query -> q -> String -> (Connection -> IO ())
+executeWrapper sqlStml tuples successMsg =
+            (\ conn ->  do 
+                        execute conn sqlStml tuples
+                        putStrLn successMsg
+            )
+      
+-- now,
+addUserNew2 :: String -> IO ()
+addUserNew2 user = withConn "tools.db" $
+                  executeWrapper "INSERT INTO users (username) VALUES (?)" (Only user) "added the user"
+
+
+checkoutMy :: Int -> Int -> IO ()
+checkoutMy userId toolId = withConn "tools.db" $
+        executeWrapper  "INSERT INTO checkedout (user_id,tool_id) VALUES (?,?)" 
+                        (userId,toolId) 
+                        "Checked out the tool"
+
+                             
+
+
+
+checkout :: Int -> Int -> IO ()
+checkout userId toolId = withConn "tools.db" $
+                         \conn -> do
+                           execute conn
+                             "INSERT INTO checkedout (user_id,tool_id) VALUES (?,?)"
+                             (userId,toolId)
+
+                          
+
 
 
 
