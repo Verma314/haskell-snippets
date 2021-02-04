70730ba9c4a93b71c01279a29dfee02c12c8572e
Aditya Verma <asverma314@gmail.com>
2021-02-04 22:56:45 +0530

    restarting monads for more depth

---
+++ 51monads.hs
@@ -0,0 +1,5 @@
+--import qualified Data.ByteString.Char8 as BC
+import qualified Data.ByteString.Lazy.Char8 as L8
+import qualified Data.ByteString.Lazy as L
+bcInt :: L8.ByteString
+bcInt =   L8.pack "abc" 
